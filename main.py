88a299f99f4babbc1c798a00152179d621a633a7
whyyes-max <54863404+whyyes-max@users.noreply.github.com>
2021-10-21 20:59:04 +0000

    napnap raprap (trianon lejárati idő 2020)

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import io
 
 app = Flask(__name__)
 
-with io.open("capitals.txt", mode="r", encoding="utf-8") as f :
+with io.open("kerulets.txt", mode="r", encoding="utf-8") as f :
     capitals = f.read().split("\n")
 
 def listInStr(list, str) :
@@ -24,23 +24,23 @@ def registerHandler() :
     pswd = request.form["password"]
 
     if not len(usr) > 2 :
-        return render_template("register.html", error="Your username must be at least 3 characters long..", usr=usr, pswd=pswd)
+        return render_template("register.html", error="neved 3 karakter hossz..", usr=usr, pswd=pswd)
 
     if not len(pswd) > 2 :
-        return render_template("register.html", error="Your password must be at least 3 characters long..", usr=usr, pswd=pswd)
+        return render_template("register.html", error="szavad 3 karakter hossz..", usr=usr, pswd=pswd)
 
     if len(pswd) > 32 :
-        return render_template("register.html", error="Your password cannot be longer than 32 characters for no reason whatsoever..", usr=usr, pswd=pswd)
+        return render_template("register.html", error="szavad 365 (nap trianon végéig) karakter hossz-nál több..", usr=usr, pswd=pswd)
 
-    if not listInStr(["0","1","2","3","4","5","6","7","8","9"], pswd) :
-        return render_template("register.html", error="Your password must include at least 1 number", usr=usr, pswd=pswd)
+    if not listInStr(["1","2","3","4","5","6","7","8","9"], pswd) :
+        return render_template("register.html", error="kell egy number ami nem 0", usr=usr, pswd=pswd)
     
     if not listInStr(["?","!","#","@"], pswd) :
-        return render_template("register.html", error="Your password must include at least 1 of theese characters: ?!#@", usr=usr, pswd=pswd)
+        return render_template("register.html", error="ezekből jelszó egy & több: ?!#@", usr=usr, pswd=pswd)
     
     if not listInStr(capitals, pswd) :
-        return render_template("register.html", error="Your password must include at least 1 capital", usr=usr, pswd=pswd)
+        return render_template("register.html", error="legyen egy kerület jelszóban", usr=usr, pswd=pswd)
 
-    return "<h3>You are now registered..</h3>"
+    return "<h3>istenáldmegamagyart bass boosted..</h3>"
 
 app.run(debug=False, host="0.0.0.0", port=5000) 
